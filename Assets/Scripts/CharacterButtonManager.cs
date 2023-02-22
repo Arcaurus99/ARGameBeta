3f1dedabceeb3d48e566a5b65975d27e92e865fe
Ark99 <32331814+Arcaurus99@users.noreply.github.com>
2023-02-22 18:57:07 -0500

    Text Load Working
    ParacitesScene - Eddited:
    The text is load properly in the UI now is needed to fix some details.

--- Assets/Scripts/CharacterButtonManager.cs
+++ Assets/Scripts/CharacterButtonManager.cs
@@ -7,24 +7,34 @@ using System;
 
 public class CharacterButtonManager : MonoBehaviour
 {
+    // Character Variables
     private string characterName;
     private string characterDescription;
     private Sprite characterImage;
     private GameObject character3DModel;
 
+    // Character Data Value
     public string CharacterName { set => characterName = value; }
     public string CharacterDescription { set => characterDescription = value; }
     public Sprite CharacterImage { set => characterImage = value; }
     public GameObject Character3DModel { set => character3DModel = value; }
 
+    // UI GameObjects
     public GameObject modelParent;
     private GameObject model;
+    private GameObject paraciteName;
+    private GameObject paraciteDesc;
 
-    public float speed = 20f;
+    // Text Components
+    Text txtParaciteName;
+    Text txtParaciteDesc;
+
+    //public float speed = 20f;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Button Creation
         transform.GetChild(0).GetComponent<RawImage>().texture = characterImage.texture;
 
         var button = GetComponent<Button>();
@@ -32,24 +42,37 @@ public class CharacterButtonManager : MonoBehaviour
         button.onClick.AddListener(Create3DModel);
     }
     
+    void Update()
+    {
+    }
+
     private void LoadInfo()
     {
+
+        paraciteName = GameObject.Find("UIManager/MainPanel/Info-panel/txt-paraciteName");
+        paraciteDesc = GameObject.Find("UIManager/MainPanel/Info-panel/txt-description");
+
+        txtParaciteName = paraciteName.GetComponent<Text>();
+        txtParaciteDesc = paraciteDesc.GetComponent<Text>();
         
+        txtParaciteName.text = characterName;
+        txtParaciteDesc.text = characterDescription;
     }
 
     private void Create3DModel()
     {
         
-        /*if (obj3DModelParent != null)
-        {
-            Destroy(obj3DModelParent);
-            Create(obj3DModelParent);
-        }*/
+        
         //modelParent = GameObject.Find("UIManager/MainPanel/ModelParent");
+        /*if (ModelParent != null)
+        {
+            Destroy(ModelParent);
+            Create(ModelParent);
+        }*/
         model = Instantiate(character3DModel);
         //model.transform.position = modelParent.transform.position;
         //model.transform.parent = modelParent.transform;
-        character3DModel.transform.Rotate(Vector3.up * Time.deltaTime * speed);
+        //character3DModel.transform.Rotate(Vector3.up * Time.deltaTime * speed);
     }
 
     /*private void SetCharacterPosition()

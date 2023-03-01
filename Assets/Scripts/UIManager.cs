36a7de5a4b78077d9d0e80218b286750aa28207b
Ark99 <32331814+Arcaurus99@users.noreply.github.com>
2023-03-01 18:00:27 -0500

    Details Menu On Game Again
    ParacitesScene / Detail Menu - Working:
    Looking the error I found the fail in the reference of the menu, so I fixed.
    
    Parasite Scene / Button More - In Process:
    It's working properly based in the DoTween lybrary capacity but not with the canvas UI.
    
    Pendding:
    1. Animation correction.
    2. Default info load in Info Panel.
    3. Delete models when they are update.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,7 +8,8 @@ using System;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private GameObject mainPanel;
-    [SerializeField] private GameObject detailsMenuPanel; //DetailsMenu probably name exception
+    [SerializeField] private GameObject detailsMenuPanel;
+    [SerializeField] private GameObject btnMore;
     
     // Start is called before the first frame update
     void Start()
@@ -25,13 +26,17 @@ public class UIManager : MonoBehaviour
 
     private void ActivateParacite()
     {
-        mainPanel.transform.GetChild(4).transform.DOScale(new Vector3(0,1,1), 0.3f);
-        //mainPanel.transform.GetChild(4).transform.DOMoveX(1, 0.3f);
+        //mainPanel.transform.GetChild(5).transform.DOScale(new Vector3(0,1,1), 0.3f); # it's the same
+        detailsMenuPanel.transform.DOScale(new Vector3(0,1,1), 0.3f);
+        //mainPanel.transform.GetChild(2).transform.DOMoveX(26, 0.3f);
+        btnMore.transform.DOMoveX(26, 0.3f);
     }
 
     private void ActivateDetailsMenu()
     {
-        mainPanel.transform.GetChild(4).transform.DOScale(new Vector3(1,1,1), 0.3f);
-        //mainPanel.transform.GetChild(4).transform.DOMoveX(0, 0.3f);
+        //mainPanel.transform.GetChild(5).transform.DOScale(new Vector3(1,1,1), 0.3f);
+        detailsMenuPanel.transform.DOScale(new Vector3(1,1,1), 0.3f);
+        //mainPanel.transform.GetChild(2).transform.DOMoveX(28, 0.3f);
+        btnMore.transform.DOMoveX(28, 0.3f);
     }
 }
